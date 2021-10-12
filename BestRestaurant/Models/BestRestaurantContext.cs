6000ae57e9b7cbdcadb79e489b9b6d379202000d
Carlos Urquiza <webquiza@users.noreply.github.com>
2021-10-11 17:16:27 -0700

    Initial commit

---
+++ BestRestaurant/Models/BestRestaurantContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BestRestaurant.Models
+{
+  public class BestRestaurantContext : DbContext
+  {
+    public DbSet<Cuisine> Cuisines { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+
+    public BestRestaurantContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
